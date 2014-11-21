e4f1667977baef03299181bb4f40916869cc4739
Cotizo Sima <simacotizo@gmail.com>
2014-11-21 12:53:38 +0100

    adding tweeting capability

--- hnot.js
+++ hnot.js
@@ -1,6 +1,19 @@
-var crawler = require('./crawler');
+var crawler = require('./crawler'),
+    config = require('./config.json'),
+    twitter = require('node-twitter'),
+    T = new twitter.RestClient(
+        config["consumerKey"],
+        config["consumerSecret"],
+        config["token"],
+        config["tokenSecret"]);
+
 
 crawler("https://news.ycombinator.com/", function (err, title, url) {
     if (err) { console.error(err); return; }
-    console.log(title + " -> " + url);
+    T.statusesUpdate({
+        status: title + ": " + url
+    }, function (err, data) {
+        if (err) { console.error(err); return; }
+        console.log(data);
+    });
 });
