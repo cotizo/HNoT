eca951dfcc10c6d0068e073881db46a65e91b1ad
Cotizo Sima <simacotizo@gmail.com>
2014-11-23 15:15:16 +0100

    using redis to store last tweet

--- filter.js
+++ filter.js
@@ -1,10 +1,27 @@
-
+var redis = require('redis');
 
 module.exports = function (config) {
     var lastValue = null;
+    var rc = null;
+    var redisKey = null;
+    if (config.hasOwnProperty("redis")) {
+        rc = redis.createClient(
+            config["redis"]["port"],
+            config["redis"]["host"]);
+        redisKey = config["redis"]["key"];
+    }
+
     return function (value, callback) {
-        var result = ! (lastValue === value);
-        lastValue = value;
-        callback(null, result);
+        if (rc) {
+            rc.get(redisKey, function (err, lastValue) {
+                var result = ! (lastValue === value);
+                rc.set(redisKey, value);
+                callback(null, result);
+            });
+        } else {
+            var result = ! (lastValue === value);
+            lastValue = value;
+            callback(null, result);
+        }
     }
 };
